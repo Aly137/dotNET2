482290b66cde2d6a64aa367649bea999b2715367
aly137 <gongolea@gmail.com>
2019-06-01 15:51:36 +0300

    1st try

---
+++ lab2/lab2/Controllers/ExpensesController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+
+        private ExpensesDbContext context;
+        public ExpensesController(ExpensesDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Flowers
+        [HttpGet]
+        public IEnumerable<Expense> Get([FromQuery]DateTime? from, [FromQuery]DateTime? to, [FromQuery]ExpenseType? type)
+        {
+            IQueryable<Expense> result = context.Expenses;
+            if (from == null && to == null && type == null)
+            {
+                return result;
+            }
+            if (from != null)
+            {
+                result = result.Where(e => e.Date >= from);
+            }
+            if (to != null)
+            {
+                result = result.Where(e => e.Date <= to);
+            }
+            if (type != null)
+            {
+                result = result.Where(e => e.ExpenseType == type);
+            }
+
+            return result;
+        }
+
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var existing = context.Expenses.FirstOrDefault(expense => expense.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(existing);
+        }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public void Post([FromBody] Expense expense)
+        {
+            context.Expenses.Add(expense);
+            context.SaveChanges();
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Expense expense)
+        {
+            var existing = context.Expenses.AsNoTracking().FirstOrDefault(e => e.Id == id);
+            if (existing == null)
+            {
+                context.Expenses.Add(expense);
+                context.SaveChanges();
+                return Ok(expense);
+            }
+            expense.Id = id;
+            context.Expenses.Update(expense);
+            context.SaveChanges();
+            return Ok(expense);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = context.Expenses.FirstOrDefault(expense => expense.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+            context.Expenses.Remove(existing);
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ lab2/lab2/Models/ExpensesDbSeeder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    public class ExpensesDbSeeder
+    {
+
+        public static void Initialize(ExpensesDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any products.
+            if (context.Expenses.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Expenses.AddRange(
+                new Expense
+                {
+                  Description = "Smart TV",
+                  Sum = 3400,
+                  Location = "Media Galaxy",
+                  Date = DateTime.Parse("25/05/2019 13:23"),
+                  Currency = "RON",
+                  ExpenseType = ExpenseType.electronics,
+                },
+                new Expense
+                {
+                    Description = "Shoes",
+                    Sum = 320,
+                    Location = "Office Shoes",
+                    Date = DateTime.Parse("03/05/2019 20:45"),
+                    Currency = "RON",
+                    ExpenseType = ExpenseType.clothes,
+                },
+                 new Expense
+                 {
+                     Description = "Rabbit",
+                     Sum = 25,
+                     Location = "Pet Shop",
+                     Date = DateTime.Parse("030/05/2019 14:30"),
+                     Currency = "Euro",
+                     ExpenseType = ExpenseType.other,
+                 }
+            );
+            context.SaveChanges();
+        }
+    }
+}
